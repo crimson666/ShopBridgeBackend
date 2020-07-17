8a6431a9e9dcee52a74994586a4d73045dc12e3c
crimson666 <ankurbsws@gmial.com>
2020-07-17 21:45:27 +0530

    final push

--- shopBridge/src/main/resources/data.sql
+++ shopBridge/src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into PRODUCTS values (1,'Avi-8','Watch','Its good Watch','AVI-8 cal44m21',1200);
 insert into PRODUCTS values (2,'Avi-8','Watch','Its good Watch','AVI-8 cal44m22',1100);
 insert into PRODUCTS values (3,'Avi-8','Watch','Its good Watch','AVI-8 cal44m23',1300);
+insert into PRODUCTS values (4,'Omega','Automatic Watch','First and only watch worn on the moon.','Omega Speedmaster',130000);
+insert into PRODUCTS values (5,'Seiko','Pilot Watch','Most Affordable quality automatic watch with in house movement.','Seiko 5 Astro',13000);
+insert into PRODUCTS values (6,'Jager lecourt','Automatic Watch','Under-decorated yet elegant watch you will ever see','Jager lecourt rotal oak',7200000);
